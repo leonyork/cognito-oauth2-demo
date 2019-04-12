ffe15151c7b2802fb688b56fe9d7e0e4d01ae9a9
Leon York <leon@leonyork.com>
2019-04-12 23:47:30 +0100

    Remove git module

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    <b-jumbotron header="Cognito" lead="Testing the OAuth2 flow with Cognito">
+    <b-jumbotron header="Cognito" :lead="visibleMessage">
       <div class="mt-3" v-if="!accessToken">
         <b-button @click="login">Login</b-button>
       </div>
       <div v-if="accessToken && !userInfo" class="loader"></div>
       <transition name="fade">
-        <b-card v-if="userInfo" title="User Info" sub-title="/oauth2/userInfo">
+        <b-card
+          v-if="userInfo"
+          title="Store a welcome message"
+          :sub-title="'PUT /user/' + userInfo.sub"
+        >
+          <b-card-text>
+            <b-form @submit="save">
+              <div class="row">
+                <div class="col-sm-10">
+                  <b-form-input
+                    v-model="message"
+                    required
+                    placeholder="Enter a message"
+                    style="width: 100%;"
+                  ></b-form-input>
+                </div>
+                <div class="col-sm-2">
+                  <b-button type="submit" variant="primary" style="width: 100%;">Save</b-button>
+                </div>
+              </div>
+            </b-form>
+          </b-card-text>
+        </b-card>
+      </transition>
+      <transition name="fade">
+        <b-card v-if="userInfo" title="User Info" sub-title="GET /oauth2/userInfo">
           <b-card-text>
             <b-list-group>
               <b-list-group-item class="d-flex justify-content-between align-items-center">
@@ -39,16 +64,28 @@ export default {
   name: "Login",
   data: function() {
     return {
-      userInfo: ""
+      userInfo: "",
+      message: "",
+      visibleMessage: "Testing the OAuth2 flow with Cognito"
     };
   },
   mounted: async function() {
     if (accessToken()) {
-      const response = await axios.get(
+      let response = await axios.get(
         `https://${process.env.VUE_APP_COGNITO_HOST}/oauth2/userInfo`,
         { headers: { Authorization: `Bearer ${accessToken()}` } }
       );
       this.userInfo = response.data;
+
+      try {
+        response = await axios.get(
+          `${process.env.VUE_APP_USER_API_URL}/user/${this.userInfo.sub}`,
+          { headers: { Authorization: `Bearer ${accessToken()}` } }
+        );
+        this.visibleMessage = response.data.message;
+      } catch (err) {
+        if (err.response.status != 404) { throw err; }
+      }
     }
   },
   methods: {
@@ -65,6 +102,14 @@ export default {
       }/logout?client_id=${
         process.env.VUE_APP_CLIENT_ID
       }&logout_uri=${encodeURI(process.env.VUE_APP_REDIRECT_URL)}`;
+    },
+    save: async function() {
+      const response = await axios.put(
+        `${process.env.VUE_APP_USER_API_URL}/user/${this.userInfo.sub}`,
+        { message: this.message },
+        { headers: { Authorization: `Bearer ${accessToken()}` } }
+      );
+      this.visibleMessage = response.data.message;
     }
   },
   computed: {
@@ -97,7 +142,11 @@ export default {
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
